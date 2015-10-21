18625b59cd779f386c64392c9710c7df54a96850
Maxime Tricoire <max.tricoire@gmail.com>
2015-10-21 15:38:07 +0200

    update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,29 @@
 var metalsmithConfig = require('./metalsmith.json');
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     grunt.initConfig({
         watch: {
             scripts: {
                 files: ['src/{,**/}*.*'],
                 tasks: ['metalsmith'],
                 options: {
-                    livereload: true
+                    livereload: '<%= connect.options.livereload %>'
                 }
             },
+        },
+
+        connect: {
+            options: {
+                port: 9000,
+                hostname: '0.0.0.0',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: 'build'
+                }
+            }
         },
 
         metalsmith: {
@@ -18,5 +32,9 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-metalsmith');
+
+    grunt.registerTask('default', ['metalsmith']);
+    grunt.registerTask('serve', ['connect:livereload', 'watch']);
 };
